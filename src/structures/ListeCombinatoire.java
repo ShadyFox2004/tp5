f7443b36ccf91a679d0b483b5f9c8dd0804cf25d
WildSource <ilovetrap123@hotmail.com>
2022-05-02 13:36:25 -0400

    added produireListeCombinatoire and an empty test suite

--- src/structures/ListeCombinatoire.java
+++ src/structures/ListeCombinatoire.java
@@ -2,187 +2,208 @@ package structures;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import exceptions.ConstructeurException;
 
 /**
  * Classe qui permet de produire une liste de listes de combinaison d'entiers
  * sans remise sans tenir compte de l'ordre à partir d'une liste d'entiers de
  * départ.
- *
+ * <p>
  * Voir les combinatoires sur "Wikipédia" :
  * https://fr.wikipedia.org/wiki/Combinatoire
  *
- * @author Vos noms
+ * @author Samuel Nguyen-Phok
  */
 public class ListeCombinatoire
 {
-	public static final int VALEUR_ENS_MIN = 0;
-	public static final int VALEUR_ENS_MAX = 50;
-	public static final int LONGUEUR_COMBINAISON_MIN = 1;
+    public static final int VALEUR_ENS_MIN = 0;
+    public static final int VALEUR_ENS_MAX = 50;
+    public static final int LONGUEUR_COMBINAISON_MIN = 1;
 
-	private int debutEns = 0;
-	private int finEns = 0;
-	private int longCombinaison = 0;
-	private List<List<Integer>> listeDeCombinaisons = null;
-	private List<Integer> ensembleValeurs = null;
+    private int debutEns = 0;
+    private int finEns = 0;
+    private int longCombinaison = 0;
+    private List<List<Integer>> listeDeCombinaisons = null;
+    private List<Integer> ensembleValeurs = null;
 
-	/**
-	 * Valide et initialise les attributs de la classe. Il génère le vecteur de
-	 * valeurs selon les bornes reçues. Il génère les listes combinatoires en
-	 * respectant la longueur voulue et le vecteur de valeurs.
-	 *
-	 * @param pValDebut la borne min pour l'ensemble des valeurs
-	 * @param pValFin la borne max pour l'ensemble des valeurs
-	 * @param pLongCombinaison la longueur des combinaisons
-	 *
-	 * @throws ConstructeurException
-	 */
-	// TODO ListeCombinatoire - Compléter le code de la méthode
-	public ListeCombinatoire(int pValDebut, int pValFin, int pLongCombinaison)
-			throws ConstructeurException
-	{
-	}
+    /**
+     * Valide et initialise les attributs de la classe. Il génère le vecteur de
+     * valeurs selon les bornes reçues. Il génère les listes combinatoires en
+     * respectant la longueur voulue et le vecteur de valeurs.
+     *
+     * @param pValDebut        la borne min pour l'ensemble des valeurs
+     * @param pValFin          la borne max pour l'ensemble des valeurs
+     * @param pLongCombinaison la longueur des combinaisons
+     * @throws ConstructeurException
+     */
+    // TODO ListeCombinatoire - Compléter le code de la méthode
+    public ListeCombinatoire(int pValDebut, int pValFin, int pLongCombinaison)
+            throws ConstructeurException
+    {
+    }
 
-	public int getDebutEns()
-	{
-		return debutEns;
-	}
+    public int getDebutEns()
+    {
+        return debutEns;
+    }
 
-	public int getFinEns()
-	{
-		return finEns;
-	}
+    public int getFinEns()
+    {
+        return finEns;
+    }
 
-	public int getLongCombinaison()
-	{
-		return longCombinaison;
-	}
+    public int getLongCombinaison()
+    {
+        return longCombinaison;
+    }
 
-	private List<Integer> getEnsembleValeurs()
-	{
-		return ensembleValeurs;
-	}
+    private List<Integer> getEnsembleValeurs()
+    {
+        return ensembleValeurs;
+    }
 
-	public List<Integer> getCombinaison(int index)
-	{
-		return listeDeCombinaisons.get(index);
-	}
+    public List<Integer> getCombinaison(int index)
+    {
+        return listeDeCombinaisons.get(index);
+    }
 
-	private List<List<Integer>> getListeDeCombinaisons()
-	{
-		return listeDeCombinaisons;
-	}
+    private List<List<Integer>> getListeDeCombinaisons()
+    {
+        return listeDeCombinaisons;
+    }
 
-	public int getTailleListeDeCombinaisons()
-	{
-		return listeDeCombinaisons.size();
-	}
+    public int getTailleListeDeCombinaisons()
+    {
+        return listeDeCombinaisons.size();
+    }
 
-	private void setLimitesEns(int pDebutEns, int pFinEns)
-	{
-		// Au besoin, inversion des valeurs pour simplement éviter les
-		// erreurs...
-		boolean ok = validerLimitesEns(pDebutEns = Math.min(pDebutEns, pFinEns),
-				pFinEns = Math.max(pDebutEns, pFinEns));
+    private void setLimitesEns(int pDebutEns, int pFinEns)
+    {
+        // Au besoin, inversion des valeurs pour simplement éviter les
+        // erreurs...
+        boolean ok = validerLimitesEns(pDebutEns = Math.min(pDebutEns, pFinEns),
+                pFinEns = Math.max(pDebutEns, pFinEns));
 
-		if (ok)
-		{
-			this.debutEns = pDebutEns;
-			this.finEns = pFinEns;
-		}
-	}
+        if (ok)
+        {
+            this.debutEns = pDebutEns;
+            this.finEns = pFinEns;
+        }
+    }
 
-	private void setLongCombinaison(int pLongCombinaison)
-	{
-		boolean ok = validerLongCombinaison(pLongCombinaison,
-				((getFinEns() - getDebutEns()) + 1));
+    private void setLongCombinaison(int pLongCombinaison)
+    {
+        boolean ok = validerLongCombinaison(pLongCombinaison,
+                ((getFinEns() - getDebutEns()) + 1));
 
-		if (ok)
-		{
-			this.longCombinaison = pLongCombinaison;
-		}
-	}
+        if (ok)
+        {
+            this.longCombinaison = pLongCombinaison;
+        }
+    }
 
-	private void setEnsembleValeurs(List<Integer> pEnsemble)
-	{
-		this.ensembleValeurs = pEnsemble;
-	}
+    private void setEnsembleValeurs(List<Integer> pEnsemble)
+    {
+        this.ensembleValeurs = pEnsemble;
+    }
 
-	private void setListeDeCombinaisons(List<List<Integer>> listeDeCombinaisons)
-	{
-		this.listeDeCombinaisons = listeDeCombinaisons;
-	}
+    private void setListeDeCombinaisons(List<List<Integer>> listeDeCombinaisons)
+    {
+        this.listeDeCombinaisons = listeDeCombinaisons;
+    }
 
-	private boolean validerLimitesEns(int pValDebut, int pValFin)
-	{
-		return ((pValDebut < pValFin)
-				&& (pValDebut >= ListeCombinatoire.VALEUR_ENS_MIN)
-				&& (pValDebut <= ListeCombinatoire.VALEUR_ENS_MAX)
-				&& (pValFin >= ListeCombinatoire.VALEUR_ENS_MIN)
-				&& (pValFin <= ListeCombinatoire.VALEUR_ENS_MAX));
-	}
+    private boolean validerLimitesEns(int pValDebut, int pValFin)
+    {
+        return ((pValDebut < pValFin)
+                && (pValDebut >= ListeCombinatoire.VALEUR_ENS_MIN)
+                && (pValDebut <= ListeCombinatoire.VALEUR_ENS_MAX)
+                && (pValFin >= ListeCombinatoire.VALEUR_ENS_MIN)
+                && (pValFin <= ListeCombinatoire.VALEUR_ENS_MAX));
+    }
 
-	private boolean validerLongCombinaison(int pLongCombinaison,
-			int pLongEnsemble)
-	{
-		return ((pLongCombinaison >= ListeCombinatoire.LONGUEUR_COMBINAISON_MIN)
-				&& (pLongCombinaison <= pLongEnsemble));
-	}
+    private boolean validerLongCombinaison(int pLongCombinaison,
+                                           int pLongEnsemble)
+    {
+        return ((pLongCombinaison >= ListeCombinatoire.LONGUEUR_COMBINAISON_MIN)
+                && (pLongCombinaison <= pLongEnsemble));
+    }
 
-	/**
-	 * Génère la liste de valeurs entre les bornes début et de fin incluses.
-	 * Considérer cet ensemble comme référence pour produire les combinaisons.
-	 *
-	 * @return une liste d'entiers entre les bornes début et fin
-	 */
-	// TODO genererEnsembleValeurs - Compléter le code de la méthode
-	public List<Integer> genererEnsembleValeurs()
-	{
-		return null;
-	}
+    /**
+     * Génère la liste de valeurs entre les bornes début et de fin incluses.
+     * Considérer cet ensemble comme référence pour produire les combinaisons.
+     *
+     * @return une liste d'entiers entre les bornes début et fin
+     */
+    // TODO genererEnsembleValeurs - Compléter le code de la méthode
+    public List<Integer> genererEnsembleValeurs()
+    {
+        return null;
+    }
 
-	/**
-	 * Permet de produire les combinaisons sans remise.
-	 *
-	 * Devrait être une méthode récursive.
-	 *
-	 * Source : Voir sur le web, plusieurs algos disponibles.
-	 *
-	 * Attention : Bien faire vos tests, c'est très facile de vous tromper...
-	 *
-	 * intéressant : http://jm.davalan.org/mots/comb/comb/combalgo.html
-	 *
-	 */
-	// TODO produireListeCombinaisons - MANDAT 1 ou 2 - Compléter le code de la
-	// méthode
-	private void produireListeCombinaisons(List<Integer> pEnsembleValeurs,
-			int pLongueurRestante, List<Integer> pCombinaisonCourante)
-	{
-		
-	}
+    /**
+     * Permet de produire les combinaisons sans remise.
+     * <p>
+     * Devrait être une méthode récursive.
+     * <p>
+     * Source : Voir sur le web, plusieurs algos disponibles.
+     * <p>
+     * Attention : Bien faire vos tests, c'est très facile de vous tromper...
+     * <p>
+     * intéressant : http://jm.davalan.org/mots/comb/comb/combalgo.html
+     */
+    private void produireListeCombinaisons(List<Integer> pEnsembleValeurs,
+                                           int pLongueurRestante, List<Integer> pCombinaisonCourante)
+    {
+        // Backup de la taille de la liste de la combinaison courante
+        int longCombinaisonCourante = pCombinaisonCourante.size();
 
-	/**
-	 * Permet de construire une chaîne de caractères structurée pour afficher
-	 * les infos d'un objet ListeCombinatoire.
-	 *
-	 * Évolue selon le développement...
-	 *
-	 *
-	 * <pre>
-	 * Exemple de sortie voulue:
-	 *
-	 * Limite de l'ensemble : [1, 20]
-	 * Longueur combinaison : 4
-	 * Ensemble : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-	 * Voici les 4845 combinaisons : [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 3, 6], [1, 2, 3, 7], ... , [16, 18, 19, 20], [17, 18, 19, 20]]
-	 *
-	 * </pre>
-	 *
-	 */
-	// TODO toString - Compléter le code de la méthode
-	@Override
-	public String toString()
-	{
-		return "";
-	}
+        for (int i = 0, longEnsembleValeurs = pEnsembleValeurs.size();
+             i < longEnsembleValeurs && pLongueurRestante <= longEnsembleValeurs - i; i++)
+        {
+            // Ajoute l'élément i à la combinaison courante
+            pCombinaisonCourante.add(pEnsembleValeurs.get(i));
+
+            // La combinaison courante est-elle complète
+            if (pLongueurRestante == 1)
+            {
+                listeDeCombinaisons.add(new ArrayList<Integer>(pCombinaisonCourante));
+            } else
+            {
+                // Demander une nouvelle combinaison avec le reste des valeurs
+                // de l'ensemble
+                produireListeCombinaisons(pEnsembleValeurs.subList(i + 1, longEnsembleValeurs),
+                        pLongueurRestante - 1, pCombinaisonCourante);
+            }
+
+            // Ajuster le contenant de la combinaison courante pour enlever le
+            // ou les valeurs de trop
+            pCombinaisonCourante = pCombinaisonCourante.subList(0, longCombinaisonCourante);
+        }
+
+        /**
+         * Permet de construire une chaîne de caractères structurée pour afficher
+         * les infos d'un objet ListeCombinatoire.
+         *
+         * Évolue selon le développement...
+         *
+         *
+         * <pre>
+         * Exemple de sortie voulue:
+         *
+         * Limite de l'ensemble : [1, 20]
+         * Longueur combinaison : 4
+         * Ensemble : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+         * Voici les 4845 combinaisons : [[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 3, 6], [1, 2, 3, 7], ... , [16, 18, 19, 20], [17, 18, 19, 20]]
+         *
+         * </pre>
+         *
+         */
+        // TODO toString - Compléter le code de la méthode
+        @Override
+        public String toString()
+        {
+            return "";
+        }
+    }
 }
---
+++ src/tests/ListeCombinatoireTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Cette classe test les methodes de la classe ListeCombinatoire
+ *
+ * @author Samuel Nguyen-Phok
+ */
+public class ListeCombinatoireTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void genererEnsembleValeurs()
+    {
+    }
+
+    @Test
+    public void testToString()
+    {
+    }
+}
