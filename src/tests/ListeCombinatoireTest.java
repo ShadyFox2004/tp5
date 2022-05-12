10d31e359b96cc023d660b2736d4660b26da264d
WildSource <ilovetrap123@hotmail.com>
2022-05-12 13:37:31 -0400

    Added small test for toString

--- src/tests/ListeCombinatoireTest.java
+++ src/tests/ListeCombinatoireTest.java
@@ -2,29 +2,38 @@ package tests;
 
 import org.junit.Before;
 import org.junit.Test;
+import structures.ListeCombinatoire;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Cette classe test les methodes de la classe ListeCombinatoire
  *
  * @author Samuel Nguyen-Phok
  */
-public class ListeCombinatoireTest
-{
+public class ListeCombinatoireTest {
+    private ListeCombinatoire smallList;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
+        smallList = new ListeCombinatoire(0, 2, 3);
     }
 
     @Test
-    public void genererEnsembleValeurs()
-    {
+    public void testInvalide() {
+
     }
 
     @Test
-    public void testToString()
-    {
+    public void genererEnsembleValeurs() {
+    }
+
+    @Test
+    public void testToString() {
+        System.out.println(smallList.toString());
+        assertEquals("Limite de l'ensemble : [0, 2]\n" +
+                "Longueur combinaison : 3\n" +
+                "Ensemble : [0, 1, 2]\n" +
+                "Voici les 1 combinaisons : [[0, 1, 2]]", smallList.toString());
     }
 }
